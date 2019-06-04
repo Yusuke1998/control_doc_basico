eb9e96edd45f792f6740477972f37e2c3c870527
Yusuke1998 <jhonnyjose1998@gmail.com>
2019-06-03 20:07:40 -0400

    Nombres en español para las rutas de acceso

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,27 @@ Route::get('documentos',function(){
 Route::get('reporte',function(){
 	return view('reportes.document');
 });
-Auth::routes();
+// Auth::routes();
+// RUTAS DE ACCESO
+	// Authentication Routes...
+Route::get('acceder', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('acceder', 'Auth\LoginController@login');
+Route::post('salir', 'Auth\LoginController@logout')->name('logout');
+
+	// Registration Routes...
+Route::get('registrar', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('registrar', 'Auth\RegisterController@register');
+
+	// Password Reset Routes...
+Route::get('contraseña/resetear', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+
+Route::post('contraseña/correo', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('contraseña/resetear/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+Route::post('contraseña/resetear', 'Auth\ResetPasswordController@reset');
+// RUTAS DE ACCESO
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('Direccion-de-Informatica',function(){
 	return [
